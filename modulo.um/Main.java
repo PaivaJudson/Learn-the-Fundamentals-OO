344a53393c11ea4509432f3c79f4e22218dbeb49
Judson Paiva <judson.paiva@hotmail.com>
2021-12-07 20:25:59 +0100

    métodos estáticos e classes

--- modulo.um/Main.java
+++ modulo.um/Main.java
@@ -1,7 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        
-        
+        int lista[] = {1, 2, 3, 4, 5, 11, 6, -1, 10, 9};
+        triangular(10);
+        minMax(lista);
+    }
 
+    public static void triangular(int n){ 
+        for(int i = 1; i<=n; i++){
+            int mul = i*(i+1)*(i+2);
+            System.out.print(mul+"  ");
+        }
+    }
+
+    public static void minMax(int lista[]){
+        int max = lista[0];
+        int min = lista[0];
+        
+        for(int num: lista){
+            if(num > max){
+                max = num;
+            }
+            if (num < min){
+                min = num;
+            }
+        }
+        System.out.println("Maior = "+max+" Mínimo = "+min);
     }
 }
