7b2db2925cd1117ce0e18f9ce24d7a585943d732
Judson Paiva <judson.paiva@hotmail.com>
2021-12-09 23:20:12 +0100

    alterácão no main

--- modulo.um/Main.java
+++ modulo.um/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        
+    }
+
+    public static void chamar(){
         int lista[] = {1, 2, 3, 4, 5, 11, 6, -1, 10, 9};
         triangular(10);
         minMax(lista);
